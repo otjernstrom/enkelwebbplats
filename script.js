6203c08be9cd292a8777f9593f720b5a78b3223b
otjernstrom <olivia.tjernstrom@hotmail.se>
2023-05-04 12:49:26 +0200

    v.2

---
+++ script.js
@@ -0,0 +1,9 @@
+function byttillandra() {
+document.getElementById("flod1").src="img/flod.jpeg";
+}
+function byttillfirst() {
+document.getElementById("flod1").src ="img/river.JPG";
+}
+function transparent() {
+    document.getElementById("extra").style.opacity = "0.5";
+  }
